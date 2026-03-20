797d9c588a49e612ba4074dcab12840b2cd5543d
genarLoya <genarodour2104@gmail.com>
2026-03-19 19:08:19 -0700

    fix: improving image loading

--- src/app/products/_components/ProductItem.tsx
+++ src/app/products/_components/ProductItem.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { IProduct } from "~/models/products";
 
 export function ProductItem({
@@ -9,7 +10,14 @@ export function ProductItem({
 }) {
   return (
     <button onClick={onClick} className="rounded-2xl border p-4 shadow">
-      <img src={product.image} alt={product.name} className="mb-3 rounded-xl" />
+      <Image
+        width={400}
+        height={400}
+        src={product.image}
+        alt={product.slug}
+        className="mb-3 rounded-xl"
+        unoptimized
+      />
       <h3 className="text-lg font-semibold">{product.name}</h3>
       <p className="text-sm text-gray-500">{product.category}</p>
       <p className="mt-2 font-bold">${product.price}</p>
